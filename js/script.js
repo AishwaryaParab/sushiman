642eb88e66afd29ce10817c6508050c95f014e17
= <=>
2023-11-14 21:57:58 +0530

    Included the logic to select a popular food category

--- js/script.js
+++ js/script.js
@@ -6,3 +6,13 @@ AOS.init({
     duration: 1000,
     offset: 100,
 });
+
+// selecting a popular food category
+const filterBtnEls = document.querySelectorAll('.popular-foods__filter-btn');
+
+filterBtnEls.forEach((filterBtnEl) => {
+    filterBtnEl.addEventListener('click', (e) => {
+        document.querySelector('.active')?.classList.remove('active');
+        e.target.classList.add('active');
+    });
+});
